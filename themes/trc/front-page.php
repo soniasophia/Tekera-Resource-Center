9e7e3098c086bd6934dccc6e4fcb79103961b36e
Sonia <soniasophiaw@gmail.com>
2017-06-13 14:32:24 -0700

    Set up front page template

---
+++ themes/trc/front-page.php
@@ -0,0 +1,14 @@
+<?php
+/* Template Name: Front Page */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+
+
+
+
+
+
+  <?php get_footer(); ?>
