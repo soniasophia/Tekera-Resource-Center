e42f87817e0735bbfab277d36931f06b74bc71f7
Nate Schmidt <nschmidt1@hotmail.com>
2017-06-13 14:26:15 -0700

    Added Flickity and JQuery in header and footer

--- themes/trc/footer.php
+++ themes/trc/footer.php
@@ -17,6 +17,10 @@
 		</div><!-- #page -->
 
 		<?php wp_footer(); ?>
-
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+      integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+      crossorigin="anonymous">
+    </script>
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 	</body>
 </html>
