a9132396b2e5305a34c24e037c3d1bd20749aa35
Nate Schmidt <nschmidt1@hotmail.com>
2017-06-16 13:40:44 -0700

    Adding/Styiling CTA

--- themes/trc/whoweare-temp.php
+++ themes/trc/whoweare-temp.php
@@ -8,7 +8,6 @@ get_the_title();
   </div>
 
   <div class="content">
-
     <div class="top-content container">
       <h1>
         <?php echo CFS()->get( 'title_1' ); ?>
@@ -59,4 +58,5 @@ get_the_title();
       </p>
     </div>
   </div>
+  <?php get_template_part( 'template-parts/cta'); ?>
 </div>
