1cacc8d9a46ce5a609756f7e8a0b66357ff2cb48
tmclean85 <trevor.a.mclean@gmail.com>
2017-06-15 17:26:41 -0700

    Add blog loader image to assets, style blog page at desktop width

--- themes/trc/index.php
+++ themes/trc/index.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		  <div class="blog-hero">
-			  <h1>Latest news from Tekera!</h1>	
+			  <h1>Latest news <br class="breakhide">from Tekera!</h1>	
 			</div>	
 
 		<?php if ( have_posts() ) : ?>
@@ -20,10 +20,10 @@ get_header(); ?>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 			<?php endif; ?>
-
+     <div class="blog-main-wrapper"> 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-      <div class="blog-post-box">
+      <div class="blog-post-box" >
 			<?php the_post_thumbnail('large') ?>
 			  <div class="blog-title-wrapper">
 				  <a href="<?php the_permalink() ?>"><h3><?php the_title(); ?></h3>
@@ -39,7 +39,10 @@ get_header(); ?>
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
-
+    <div class="more-blogs-button">
+			<a><h2>load more</h2></a>
+		</div>	
+		</div><!--blog-main-wrapper-->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
