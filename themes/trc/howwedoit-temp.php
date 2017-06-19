42972892c1c90fea6438aa057039c6685f0adee5
tmclean85 <trevor.a.mclean@gmail.com>
2017-06-19 13:23:56 -0700

    Build and style How We Do It page at mobile width

--- themes/trc/howwedoit-temp.php
+++ themes/trc/howwedoit-temp.php
@@ -3,3 +3,85 @@
 get_header(); 
 get_the_title();
 ?>
+<div class="how-we-do-it-page">
+  <div class="how-hero-wrapper">
+  </div><!--how-hero-wrapper-->  
+  <section class="our-model-header-box">
+    <h2>our model</h2>
+  </section><!--out-model-header-box-->  
+  <div class="our-model-content-wrapper">
+    <h2>our model</h2>
+    <p>We use a variety of approaches to ensure our development practice is:</p>
+    <section class="dev-practices">
+      <p>Holistic</p>
+      <p>Inclusive</p>
+      <p>Non-Colonial</p>
+      <p>Empowerment</p>
+    </section>  
+      <div class="how-large-text">
+        <p>We believe that supporting a community holistically 
+           through a locally driven wellbeing-based development 
+           model is the key to ending poverty, increasing health, 
+           and growing economically. Our 5 unique programs intersect 
+           with one another to create a complementary and supportive 
+           framework, ensuring not only inclusion for all members of 
+           our community, but also an opportunity to thrive. We also 
+           focus on a holistic community approach to development. 
+           This is important, as health and wellbeing are intrinsically
+           complicated. We address this in all of our programming 
+           through incorporating a social determinants of health 
+           approach. This approach looks at how factors such as 
+           economics, gender, health and wellbeing, education, culture,
+           early childhood development, age, class, social inclusion, 
+           food security, race, and (dis)ability all factor into 
+           decreasing inequity and inequality. ICEF considers all of 
+           these when collaborating to implement programs at TRC.
+        </p>
+        <p class="pesos-content">The Tekera Pesos</p>  
+        <p>In order to give real worth to the services offered 
+           by the Centre, this local currency was developed by 
+           TRC not long after the Centre opened. The Community 
+           Work Programme and the Peso took some time to 
+           establish, as initially the Centre operated on a 
+           ‘promise to work’ concept which was difficult to 
+           manage.  The Clinic actually closed down for a week in
+           April 2006 because the concept of the Work Programme 
+           was untenable.  The founders of TRC then brainstormed 
+           ways to make the CWP more effective.   As a result, the 
+           Tekera Peso was introduced. The Centre has since worked 
+           hard at maintaining the value of the Peso by continuing 
+           to be strict about Peso payments and ensuring that Pesos 
+           are only awarded to those who have worked one full hour.
+           Pesos earned by working at the Centre in the Community Work 
+           Programme give the volunteers a sense of being stakeholders 
+           in the project.  Community members do small jobs including 
+           work on the community farm, maintaining the Centre compound, 
+           or repairing designated areas in the community such as roads, 
+           tracks and often-used paths. Earning 1 peso per hour or task, 
+           it holds a value of 500/= Ugandan shillings (.25 cents Cnd) 
+           and can be used as currency in exchange for medical treatment
+           or medications in the clinic.  An average cost of treatment 
+           at the clinic is approximately  2500 UGX (about $1 dollar).
+           Today, the Peso has also created a second economy in the 
+           village.  There people can buy and sell Pesos as needed, as 
+           well as trade the Peso locally in exchange for basic needs 
+           like sugar or salt.          
+        </p>  
+      </div><!--how-large-text-->     
+  </div><!--our-model-content-wrapper-->
+    <div class="how-we-do-it-prefooter">
+      <div class="how-prefooter-overlay">
+        <div class="prefooter-overlay-header">   
+          <h1>You can be part of the change!</h1>
+          <p>There are many ways you can contribute to the 
+             Tekera Resource Centre and help to build a better 
+             future for Ugandans!
+          </p> 
+          <div class="get-involved-link">
+            <h2>get involved</h2>
+          </div>    
+        </div><!--prefooter-overlay-header-->     
+      </div><!--how-prefooter-overlay-->         
+    </div><!--how-we-do-it-prefooter--> 
+    <?php get_footer(); ?>
+</div><!--how-we-do-it-page-->
