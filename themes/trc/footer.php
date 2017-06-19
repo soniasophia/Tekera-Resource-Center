9108b10e5d02d112bfb525b3a6712453f19f0757
tmclean85 <trevor.a.mclean@gmail.com>
2017-06-18 22:55:58 -0700

    Add hover styling to footer link elements

--- themes/trc/footer.php
+++ themes/trc/footer.php
@@ -13,16 +13,16 @@
         <div class="footer-main-wrapper">
 					<div class="footer-top">
 					  <div class="footer-box" id="who">
-					  	<h2>who we are</h2>
-					  	<p>Our Values</p>
-					  	<p class="hider">Our History</p>
-					    <p class="hider">Our Members</p>
+					  	<h2><a href="">who we are</a></h2>
+					  	<p><a href="">Our Values</a></p>
+					  	<p class="hider"><a href="">Our History</a></p>
+					    <p class="hider"><a href="">Our Members</a></p>
 					  </div><!--footer-box-->	
 					  <div class="footer-box" id="how">
-					  	<h2>how we do it</h2>
-					  	<p>Holistic Approach</p>
-					  	<p class="hider">The 'Tekera Pesos'</p>
-					  	<p>Transparency</p>
+					  	<h2><a href="">how we do it</a></h2>
+					  	<p><a href="">Holistic Approach</a></p>
+					  	<p class="hider"><a href="">The 'Tekera Pesos'</a></p>
+					  	<p><a href="">Transparency</a></p>
 					  </div><!--footer-box-->	
 					  <div class="social-media-box">
 					  	<h2>follow our stories</h2>
@@ -35,22 +35,22 @@
 					</div><!--footer-top-->	
 					<div class="footer-bottom">
 					  <div id="what" class="footer-box">
-              <h2>what we do</h2>
-					  	<p class="hider">Education</p>
-					  	<p class="hider">Co-Operative Farm</p>
-					  	<p class="hider">Healthcare</p>
-					  	<p class="hider">Community Work</p>
-					  	<p class="hider">Women Empowerment</p>
-					  	<p class="hider">Economic Development</p>					
+              <h2><a href="">what we do</a></h2>
+					  	<p class="hider"><a href="">Education</a></p>
+					  	<p class="hider"><a href="">Co-Operative Farm</a></p>
+					  	<p class="hider"><a href="">Healthcare</a></p>
+					  	<p class="hider"><a href="">Community Work</a></p>
+					  	<p class="hider"><a href="">Women Empowerment</a></p>
+					  	<p class="hider"><a href="">Economic Development</a></p>					
 					  </div><!--footer-box-->
 						<div class="footer-box" id="get-involved">
-							<h2>get involved</h2>
-							<p>Sponsor</p>
-							<p>Internship</p>
-							<p>Donate</p>
+							<h2><a href="">get involved</a></h2>
+							<p><a href="">Sponsor</a></p>
+							<p><a href="">Internship</a></p>
+							<p><a href="">Donate</a></p>
 						</div><!--footer-box-->
             <div class="contact-column tablet-hider">
-							<h2>contact</h2>
+							<h2><a href="">contact</a></h2>
 							<img class="hider" id="uganda-icon" src="<?php echo get_template_directory_uri(); ?>/assets/Icons/Desktop/png/Tekera_Icon_Uganda.png">
               <img class="hider" id="canada-flag" src="<?php echo get_template_directory_uri(); ?>/assets/Icons/Desktop/png/Tekera_Icon_Canda.png">  							
             </div><!--contact-column-->
@@ -59,12 +59,12 @@
 						  <p>Tekera Resource Center</p>
 						  <p>P.O. Box 960</p>
 						  <p>Masaka, Uganda</p>
-						  <span class="contact-email hider"><p>tekeraresourcecentre@gmail.com</p></span>	
+						  <span class="contact-email hider"><p><a href="mailto:tekeraresourcecentre@gmail.com">tekeraresourcecentre@gmail.com</a></p></span>	
 							<span class="contact-spacer"><h3>Canada</h3></span>
 							<p>5730 Sherwood Blvd</p>
 							<p>Delta BC, Canada</p>
 							<p>V4L 2C6</p>	
-						  <span class="contact-email"><p>info@icefcanada.org</p></span>																													
+						  <span class="contact-email"><p><a href="mailto:info@icefcanada.org">info@icefcanada.org</a></p></span>																													
 						</div><!--contact-column-right-->	
 						<div class="contact-column-outside tablet-hider">
 						  <p class="hider">ICEF Country Director</p>
