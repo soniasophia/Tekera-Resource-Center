fb4116dc84a8a079e551247ebf15e8ded586bedd
kelvinlau <kelvinlau14@gmail.com>
2017-06-20 12:37:27 -0700

    added display none on the hamburger menu

---
+++ themes/trc/page-getinvolved.php
@@ -0,0 +1,43 @@
+<?php 
+/* Template Name: Get Involved Template */
+get_header(); 
+get_the_title();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+      <div class = "banner">
+      </div>
+
+      <div class = "overview-container">
+        <p> overview</p>
+
+        <div class = "overview-wrapper">
+          <p> get involved with trc </p>
+
+            <div class = "sponsor">
+              <h1>Sponsor</h1>
+              <p>TRC has always strived to pride high quality education in a village setting. As such, we have done sour best to make attending school as cost effective and affordable as we can. You sponsorships covers all school fees, school requirements, and learning materials of students.</p>
+              <button type = "button" class = "button"> Learn More </button>
+            </div>
+
+            <div class = "internship">
+              <h1>Internships</h1>
+              <p> In alignment with our goal towards sustainability, TRC is at the stage whereby we do not "need" volunteers to achieve our daily work. We have Ugandan professionals who have proven to be very capable in thier fields, we do, however, welcome anyone willing to help us, expandn our skills and contribute theirs. </p>
+              <button type = "button" class = "button"> Learn More </button>
+            </div>
+
+            <div class = "donate">
+              <h1> Donate</h1>
+              <p>TRC believes in a sustainable model for progressive community growth. Our low administration fees(less than 1% since 2008) ensure that your full contribution will result in real chaneg at the TRC. </p>
+              <button type = "button" class = "button"> Learn More </button>
+            </div>
+        </div>
+      </div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
