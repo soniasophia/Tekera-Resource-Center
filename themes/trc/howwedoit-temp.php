4373666b7384c8d773988fcf6af17ef31a9c8369
tmclean85 <trevor.a.mclean@gmail.com>
2017-06-20 08:50:51 -0700

    Additional styling

--- themes/trc/howwedoit-temp.php
+++ themes/trc/howwedoit-temp.php
@@ -3,6 +3,7 @@
 get_header(); 
 get_the_title();
 ?>
+<?php wp_nav_menu( array( 'theme_location' => 'what-we-do-sub-menu', 'items_wrap' => '<ul id="%1$s" class="%2$s mobile-top-nav">%3$s</ul>', 'container' => 'div', 'container_class' => 'custom-sub-menu-wrapper' ) ); ?>
 <div class="how-we-do-it-page">
   <div class="how-hero-wrapper">
   </div><!--how-hero-wrapper-->  
