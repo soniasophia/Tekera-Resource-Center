0da2e698327019c79389025d1f82cdc6fa846e3a
tmclean85 <trevor.a.mclean@gmail.com>
2017-06-20 12:46:51 -0700

    Implement and style global goals mobile, begin flickity implementation

---
+++ themes/trc/howwedoit-child-globalgoals.php
@@ -0,0 +1,56 @@
+<?php 
+/* Template Name: Global Goals */
+get_header(); 
+get_the_title();
+?>
+<div class="global-goals-page">
+  <div class="global-goals-hero global-mobile-hero">
+  </div><!--global-goals-hero global-mobile-hero-->
+  <div class="sustainability-bar">
+    <h2>global sustainability goals</h2>
+  </div><!--sunstainability-bar-->    
+  <div class="global-goals-content-wrapper">
+    <h2>the global goals for sustainability</h2>
+    <p>
+      In September 2015, 193 world leaders agreed to 17 
+      Global Goals for Sustainable Development. If these 
+      Goals are completed, it would mean an end to extreme 
+      poverty, inequality and climate change by 2030.
+    </p>
+    <p>
+      The Tekera Resource Centre is fully commited to making 
+      all necessary efforts  to turn this into a reality. We 
+      believe it is in everyone’s hands to create a world with 
+      justice and oportunitinities to all of us and our future 
+      generations.      
+    </p>    
+  </div><!--global-goals-content-wrapper-->  
+  <div class="compliance-header">
+    <p>We currently comply with 
+       <span class="compliance-header-bold">
+         15 out of 17
+       </span>
+       Sustainable Development Goals (SDGs) that are related 
+       to us!
+    </p>   
+  </div><!--compliance-header-->  
+  <div class="pre-compliance-box">
+    <p>
+      Check out what we're doing in the SDGs that are related
+      to our location!
+    </p>  
+  </div><!--pre-compliance-box-->  
+  <div class="global-carousel" data-flickity='{ "cellAlign": "left", "contain": true }'>
+    <ul class="global-carousel-list">
+      <li>
+        <div class="carousel-sub">
+          <img src="<?php echo get_template_directory_uri();?>/assets/Icons/Sustainability Development Goals/Inverted icons/e_inverted_sdg_goals_icons_individual_rgb_01.png">
+        </div><!--carousel-sub-->  
+      </li>  
+    </ul>  
+  </div><!--global-carousel-->
+
+
+</div><!--global-goals-page-->  
+
+<?php get_footer() ?>
