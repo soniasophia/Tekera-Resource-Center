141412d08112db3e95254af11091c2d69eaea50a
Sonia <soniasophiaw@gmail.com>
2017-06-20 11:34:03 -0700

    Add dropdown menu arrow to dropdown menu

--- themes/trc/archive-projects.php
+++ themes/trc/archive-projects.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
   <header class="projects-header">
-    <h1 class="projects-title"><a href="#" id="projects-toggle-menu"><?php the_archive_title() ?></a></h1>
+      <h1 class="projects-title"><a href="#" class="dropdown-arrow" id="projects-toggle-menu"><?php the_archive_title() ?></a></h1>
 
     <?php wp_nav_menu( array( 'theme_location' => 'what-we-do-sub-menu', 'items_wrap' => '<ul id="%1$s" class="%2$s mobile-top-nav">%3$s</ul>', 'container' => 'div', 'container_class' => 'custom-sub-menu-wrapper' ) ); ?>
 
