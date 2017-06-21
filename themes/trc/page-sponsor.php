ee82fec22752b3bf8e7a458a82de3c10a3160993
Sonia <soniasophiaw@gmail.com>
2017-06-21 10:50:08 -0700

    Enable dropdown menu for Sponsor page mobile view

--- themes/trc/page-sponsor.php
+++ themes/trc/page-sponsor.php
@@ -5,20 +5,17 @@ get_header();
 get_the_title();
 ?>
 
-<?php wp_nav_menu( array( 'theme_location' => 'get-involved-sub-menu', 'items_wrap' => '<ul id="%1$s" class="%2$s mobile-top-nav">%3$s</ul>', 'container' => 'div', 'container_class' => 'custom-sub-menu-wrapper' ) ); ?>
+ <header class="sponsor-header">
+    <div class="sponsor-hero">
+    
+    	<div class="project-title-container">
+      	<h1 class="sponsor-title"><a href="#" class="dropdown-arrow" id="projects-toggle-menu"><?php the_title() ?></a></h1>
+    	</div>
 
+    <?php wp_nav_menu( array( 'theme_location' => 'get-involved-sub-menu', 'items_wrap' => '<ul id="%1$s" class="%2$s mobile-top-nav">%3$s</ul>', 'container' => 'div', 'container_class' => 'custom-sub-menu-wrapper' ) ); ?>
 
-
-
-
-
-
-
-
-
-
-
-</div>
+    </div>
+  </header>
 
 <?php 
 @include "template-parts/cta.php";
