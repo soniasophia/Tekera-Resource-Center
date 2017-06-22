5969ef7fde4f0f4e43bc9d2b509e73f21f33f31f
Nate Schmidt <nschmidt1@hotmail.com>
2017-06-21 18:27:06 -0700

    Adding in tab/acrodian javascript

--- themes/trc/taxonomy-project-type.php
+++ themes/trc/taxonomy-project-type.php
@@ -5,27 +5,47 @@ get_header(); ?>
 
   <header class="projects-header">
     <div class="project-type-hero">
-    
-    	<div class="project-title-container">
-      	<h1 class="projects-title"><a href="#" class="dropdown-arrow" id="projects-toggle-menu"><?php the_archive_title() ?></a></h1>
-    	</div>
 
-    <?php wp_nav_menu( array( 'theme_location' => 'what-we-do-sub-menu', 'items_wrap' => '<ul id="%1$s" class="%2$s mobile-top-nav">%3$s</ul>', 'container' => 'div', 'container_class' => 'custom-sub-menu-wrapper' ) ); ?>
+      <div class="project-title-container">
+        <h1 class="projects-title">
+          <a href="#" class="dropdown-arrow" id="projects-toggle-menu">
+            <?php the_archive_title() ?>
+          </a>
+        </h1>
+      </div>
+
+      <?php wp_nav_menu( array( 'theme_location' => 'what-we-do-sub-menu', 'items_wrap' => '<ul id="%1$s" class="%2$s mobile-top-nav">%3$s</ul>', 'container' => 'div', 'container_class' => 'custom-sub-menu-wrapper' ) ); ?>
 
     </div>
-  </header><!-- .page-header -->
+  </header>
+  <!-- .page-header -->
 
 
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
       <div class="container">
+        <div class="hero-banner">
+        </div>
+        <div class="tab-acord">
+          
+        
+        <ul id="nav">
+        <?php while ( have_posts() ) : the_post(); ?>
+
+        
+          <li><a href="#"><?php echo the_title();?></a>
+            <section>
+              <p><?php echo the_content(); ?></p>
+            </section>
+          </li>
+
+          <?php endwhile; ?>
+          </ul>
+          </div>
 
 
+      </div>
+    </main>
+  </div>
 
-
-
-			</div>
-		</main>
-	</div>
-
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
