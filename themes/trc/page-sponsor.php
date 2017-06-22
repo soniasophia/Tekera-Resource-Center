47b64d45a5d957653b08624f85a9cb5faf3b82e9
Sonia <soniasophiaw@gmail.com>
2017-06-22 09:58:10 -0700

    Add max width to sub nav on What We Do desktop view

--- themes/trc/page-sponsor.php
+++ themes/trc/page-sponsor.php
@@ -9,13 +9,53 @@ get_the_title();
     <div class="sponsor-hero">
     
     	<div class="project-title-container">
-      	<h1 class="sponsor-title"><a href="#" class="dropdown-arrow" id="projects-toggle-menu"><?php the_title() ?></a></h1>
+        <img id="projects-toggle-menu" class="dropdown-arrow" src="<?php echo get_template_directory_uri();?>/assets/Icons/Mobile/Mobile_png/tekera_mobile_icon_arrow_down_dark_teal.png">
+      
+      	<h1 class="sponsor-title"><?php the_title() ?></h1>
     	</div>
 
     <?php wp_nav_menu( array( 'theme_location' => 'get-involved-sub-menu', 'items_wrap' => '<ul id="%1$s" class="%2$s mobile-top-nav">%3$s</ul>', 'container' => 'div', 'container_class' => 'custom-sub-menu-wrapper' ) ); ?>
 
     </div>
   </header>
+
+  <div class="tab-acord">
+    <ul id="nav">
+      <li><a href="#">Overview</a>
+        <section>
+          <h2><?php echo CFS()->get( 'title_1' ); ?></h2>
+          <p><?php echo CFS()->get( 'content_1' ); ?></p>
+        </section>
+      </li>
+
+        <li><a href="#">Sponsor Students and Classrooms</a>
+        <section>
+          <h2><?php echo CFS()->get( 'title_2' ); ?></h2>
+          <p><?php echo CFS()->get( 'content_2' ); ?></p>
+          <h2><?php echo CFS()->get( 'title_2a' ); ?></h2>
+          <p><?php echo CFS()->get( 'content_2a' ); ?></p>
+          <h2><?php echo CFS()->get( 'title_2b' ); ?></h2>
+          <p><?php echo CFS()->get( 'content_2b' ); ?></p>
+
+        </section>
+      </li>
+
+        <li><a href="#">Sponsor a Project</a>
+        <section>
+          <p></p>
+        </section>
+      </li>
+    </ul>
+  </div>
+
+
+
+
+
+
+
+
+
 
 <?php 
 @include "template-parts/cta.php";
