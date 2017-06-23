25a299a10698a406637c9eca7fbfce9aecce1aa8
kelvinlau <kelvinlau14@gmail.com>
2017-06-23 15:34:19 -0700

    Added job post and styling to get-invloved-current-opportunity page

--- themes/trc/page-internship.php
+++ themes/trc/page-internship.php
@@ -37,9 +37,33 @@ get_the_title();
           <section>
             <h2><?php echo CFS()->get( 'title_2' ); ?></h2>
 
+            <?php
+            $args = array( 'post_type' => 'jobs', 'posts_per_page' => 3 );
+            $loop = new WP_Query( $args );?>
+            
+            <?php
+            while ( $loop->have_posts() ) : $loop->the_post();?>
+
+              <div class = "job-post">
+                <?php
+                echo '<div class="job-title">';?>
+                <?php the_title();
+                echo '</div>';
+                echo '<div class="entry-content">';
+                the_content();
+                echo '</div>';
+                echo '<button type = "button" class = "button"> Apply Now </button>';
+                ?>
+              </div>
+            <?php endwhile;
+            ?>
+
+            <div class = "job-info">
             <p>Don't see a position that fits your experience? We are always on the look out for new talent. Feek free to submit an application form describing the expertise you could bring to TRC. </p>
 
-            <!--<button type = "button" class = "button"> Apply Now </button>-->
+            <button type = "button" class = "button"> Apply Now </button>
+            </div>
+
           </section>
         </li>
 
@@ -51,6 +75,40 @@ get_the_title();
       </div>
     </ul>
   </div>
+  
+
+  <!--<div class="fund-project">
+    <h1>Fund a Project</h1>
+    <img id="mobile-dropdown-menu" class="dropdown-arrow-white" src="<?php echo get_template_directory_uri();?>/assets/Icons/Mobile/Mobile_png/tekera_mobile_icon_arrow_down_dark_teal.png">
+  </div>
+
+<div class="mobile-dropdown">
+    <div class="content-dropdown">
+      <a href="#" id="option1" class="mobile-content-toggle">Overview</a>
+      <a href="#" id="option2" class="mobile-content-toggle">Sponsor Students & Classrooms</a>
+      <a href="#" id="option3" class="mobile-content-toggle">Sponsor a Project</a>
+    </div>
+
+    <div id="content1" class="option1 content">
+      <h1><?php echo CFS()->get( 'title_1' ); ?></h1>
+      <p><?php echo CFS()->get( 'content_1' ); ?></p>
+    </div>
+
+    <div id="content2" class="option2 content">
+      <h1><?php echo CFS()->get( 'title_2' ); ?></h1>
+      <p><?php echo CFS()->get( 'content_2' ); ?></p>
+      <h2><?php echo CFS()->get( 'title_2a' ); ?></h2>
+      <p><?php echo CFS()->get( 'content_2a' ); ?></p>
+      <h2><?php echo CFS()->get( 'title_2b' ); ?></h2>
+      <p><?php echo CFS()->get( 'content_2b' ); ?></p>
+    </div>
+
+    <div id="content3" class="option3 content">
+      <h1>Sponsor a Project</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</div>-->
+
 
 
 		</main><!-- #main -->
