7fcfa9f2430996398cd8e9cc2d09c7dd4d8ff1e1
Sonia <soniasophiaw@gmail.com>
2017-06-23 09:56:48 -0700

    Style cfs tab menu

--- themes/trc/js/acordain_tab_behaviour.js
+++ themes/trc/js/acordain_tab_behaviour.js
@@ -5,7 +5,7 @@
     $('#tab-nav').on('click', 'li > a', function(event) {
       event.preventDefault();
         
-      if (!$(this).hasClass('active')) {
+      if(!$(this).hasClass('active')) {
 
         $('#tab-nav .is-open').removeClass('is-open').hide();
         $(this).next().toggleClass('is-open').toggle();
@@ -18,3 +18,26 @@
       }
    });
 })(jQuery);
+
+
+// (function ($){
+// if($('#mobile-dropdown-menu').length) {
+//   $('#mobile-dropdown-menu').click(function (event) {
+//     event.preventDefault();
+//     $('.mobile-dropdown').toggle();
+//   });
+// }
+// })(jQuery);
+
+
+// (function ($) {
+//   $('.option-1').on('click', function(event) {
+//     event.preventDefault();
+//     var menu = $(this).attr('id');
+//     console.log(menu);
+//     var menuClass = '.mobile-dropdown .content .' + menu;
+//   $('.mobile-dropdown .content').hide();
+//   $(menuClass).show();
+//   console.log(menuClass);
+// });
+// })(jQuery);
--- themes/trc/page-sponsor.php
+++ themes/trc/page-sponsor.php
@@ -22,35 +22,66 @@ get_the_title();
   <div class="tab-acord">
     <ul id="tab-nav">
       <div class="green-line">
-      <li><a href="#">Overview</a>
-        <section>
-          <h2><?php echo CFS()->get( 'title_1' ); ?></h2>
-          <p><?php echo CFS()->get( 'content_1' ); ?></p>
-        </section>
-      </li>
+        <li><a href="#">Overview</a>
+          <section>
+            <h1><?php echo CFS()->get( 'title_1' ); ?></h1>
+            <p><?php echo CFS()->get( 'content_1' ); ?></p>
+          </section>
+        </li>
 
         <li><a href="#">Sponsor Students & Classrooms</a>
-        <section>
-          <h2><?php echo CFS()->get( 'title_2' ); ?></h2>
-          <p><?php echo CFS()->get( 'content_2' ); ?></p>
-          <h2><?php echo CFS()->get( 'title_2a' ); ?></h2>
-          <p><?php echo CFS()->get( 'content_2a' ); ?></p>
-          <h2><?php echo CFS()->get( 'title_2b' ); ?></h2>
-          <p><?php echo CFS()->get( 'content_2b' ); ?></p>
-          <a id="sponsor-now-button">Sponsor Now</a>
-        </section>
-      </li>
+          <section>
+            <h1><?php echo CFS()->get( 'title_2' ); ?></h1>
+            <p><?php echo CFS()->get( 'content_2' ); ?></p>
+            <h2><?php echo CFS()->get( 'title_2a' ); ?></h2>
+            <p><?php echo CFS()->get( 'content_2a' ); ?></p>
+            <h2><?php echo CFS()->get( 'title_2b' ); ?></h2>
+            <p><?php echo CFS()->get( 'content_2b' ); ?></p>
+            <a id="sponsor-now-button">Sponsor Now</a>
+          </section>
+        </li>
 
         <li><a href="#">Sponsor a Project</a>
-        <section>
-          <p></p>
-        </section>
-      </li>
+          <section>
+            <h1>Sponsor a Project</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </section>
+        </li>
       </div>
     </ul>
   </div>
 
-  
+  <div class="fund-project">
+    <h1>Fund a Project</h1>
+    <img id="mobile-dropdown-menu" class="dropdown-arrow-white" src="<?php echo get_template_directory_uri();?>/assets/Icons/Mobile/Mobile_png/tekera_mobile_icon_arrow_down_dark_teal.png">
+  </div>
+
+<div class="mobile-dropdown">
+    <div class="content-dropdown">
+      <a href="#" id="option1" class="option-1">Overview</a>
+      <a href="#" id="option2" class="option-2">Sponsor Students & Classrooms</a>
+      <a href="#" id="option3" class="option-3">Sponsor a Project</a>
+    </div>
+
+    <div id="content1" class="option1 content">
+      <h1><?php echo CFS()->get( 'title_1' ); ?></h1>
+      <p><?php echo CFS()->get( 'content_1' ); ?></p>
+    </div>
+
+    <div id="content2" class="option2 content">
+      <h1><?php echo CFS()->get( 'title_2' ); ?></h1>
+      <p><?php echo CFS()->get( 'content_2' ); ?></p>
+      <h2><?php echo CFS()->get( 'title_2a' ); ?></h2>
+      <p><?php echo CFS()->get( 'content_2a' ); ?></p>
+      <h2><?php echo CFS()->get( 'title_2b' ); ?></h2>
+      <p><?php echo CFS()->get( 'content_2b' ); ?></p>
+    </div>
+
+    <div id="content3" class="option3 content">
+      <h1>Sponsor a Project</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </div>
 
 
 
