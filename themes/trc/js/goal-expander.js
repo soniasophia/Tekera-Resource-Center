d96a62501e626b2e6af7d940233ef1c16df27e75
tmclean85 <trevor.a.mclean@gmail.com>
2017-06-26 15:35:34 -0700

    Adjust global goals styling, remove console logs from global goals script

--- themes/trc/js/goal-expander.js
+++ themes/trc/js/goal-expander.js
@@ -7,14 +7,12 @@ jQuery(document).ready(function ($){
     event.preventDefault();
     var click = $(this).data('target');
     clickId = '#' + click;
-    console.log(clickId);
     $(selector).hide();    
     $(clickId).show();
   });
 
   $(closer).click(function (event){
     event.preventDefault();
-    console.log('progress..');
     $(clickId).hide();
     $(selector).show();
   });
