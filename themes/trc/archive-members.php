48130ac86305087fc2b0ae65e49c9f1ccc103284
Nate Schmidt <nschmidt1@hotmail.com>
2017-06-26 14:21:57 -0700

    fixed members flex bug and added in sub menu

--- themes/trc/archive-members.php
+++ themes/trc/archive-members.php
@@ -10,16 +10,25 @@ get_header(); ?>
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
+
       <?php if ( have_posts() ) : ?>
 
-      <header class="page-header">
+      <header class="members-header">
         <div class="hero-banner">
+          <div class="project-title-container">
+            <img id="projects-toggle-menu" class="dropdown-arrow" src="<?php echo get_template_directory_uri();?>/assets/Icons/Mobile/Mobile_png/tekera_mobile_icon_arrow_down_dark_teal.png">
+            <h2 class="projects-title">About Us</h2>
+          </div>
+
+
+
+          <?php wp_nav_menu( array( 'theme_location' => 'who-we-are-sub-menu', 'items_wrap' => '<ul id="%1$s" class="%2$s mobile-top-nav">%3$s</ul>', 'container' => 'div', 'container_class' => 'custom-sub-menu-wrapper' ) ); ?>
         </div>
         <h1>Our Ugandan Team</h1>
       </header>
       <!-- .page-header -->
       <div id="single-view-member">
-        
+
       </div>
       <div class="articles-members">
 
@@ -28,11 +37,11 @@ get_header(); ?>
         <?php while ( have_posts() ) : the_post(); ?>
 
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <?php if ( has_post_thumbnail() ) : ?>
-            <?php the_post_thumbnail( 'full' ); ?>
-            <?php endif; ?>
-            <div class="text">
-              <h2>
+          <?php if ( has_post_thumbnail() ) : ?>
+          <?php the_post_thumbnail( 'full' ); ?>
+          <?php endif; ?>
+          <div class="text">
+            <h2>
               <button class="members-button" type="button" value='<?php echo the_ID() ?>'>
                 <?php 
               echo CFS()->get( 'first_name' );
@@ -41,12 +50,12 @@ get_header(); ?>
             ?>
             </button>
             </h2>
-              
-              <p class="job-title">
-                <?php echo CFS()->get( 'job_title' ); ?>
-              </p>
 
-            </div>
+            <p class="job-title">
+              <?php echo CFS()->get( 'job_title' ); ?>
+            </p>
+
+          </div>
           <!-- .entry-header -->
         </article>
         <!-- #post-## -->
