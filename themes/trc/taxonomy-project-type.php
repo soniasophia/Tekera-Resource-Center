ae17b8b375b55450f0356d2220e80c3f8c6b39e5
Nate Schmidt <nschmidt1@hotmail.com>
2017-06-26 10:42:42 -0700

    added and styled nav

--- themes/trc/taxonomy-project-type.php
+++ themes/trc/taxonomy-project-type.php
@@ -8,9 +8,8 @@ get_header(); ?>
 
       <div class="project-title-container">
         <h1 class="projects-title">
-          <a href="#" class="dropdown-arrow" id="projects-toggle-menu">
+          <img id="projects-toggle-menu" class="dropdown-arrow" src="<?php echo get_template_directory_uri();?>/assets/Icons/Mobile/Mobile_png/tekera_mobile_icon_arrow_down_dark_teal.png">
             <?php the_archive_title() ?>
-          </a>
         </h1>
       </div>
 
