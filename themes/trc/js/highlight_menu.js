6ccebb4cab64fe0e8ad6cd09ddc7e41e4c6ea020
Nate Schmidt <nschmidt1@hotmail.com>
2017-06-27 16:07:29 -0700

    all menus now bold

--- themes/trc/js/highlight_menu.js
+++ themes/trc/js/highlight_menu.js
@@ -1,15 +1,23 @@
-(function($){
+(function ($) {
 
   var pathname = window.location.pathname;
   var splitPath = pathname.split('/');
   //$('menu-mobile-top-nav')
-  $('.mobile-top-nav li').each(function(){
-    splitPath.forEach(function(element) {
-      if($(this).children().text().toUpperCase().replace(/\s/g,'').replace(/-/g,'').includes(element.toUpperCase().replace(/\s/g,'').replace(/-/g,''))
-      && element != ''){
+  splitPath.forEach(function (element) {
+    if (element.toUpperCase().replace(/\s/g, '').replace(/-/g, '') == 'HOWWEDOIT' && splitPath.includes('global-goals') == false) {
+
+      $('.mobile-top-nav li:nth-child(1)').children().css("font-weight", "900");
+    }
+  });
+
+  $('.mobile-top-nav li').each(function () {
+    splitPath.forEach(function (element) {
+      if ($(this).children().text().toUpperCase().replace(/\s/g, '').replace(/-/g, '').includes(element.toUpperCase().replace(/\s/g, '').replace(/-/g, '')) &&
+        element != '') {
         $(this).css("font-weight", "900");
       }
     }, this);
+
   });
 
-  })(jQuery);
+})(jQuery);
